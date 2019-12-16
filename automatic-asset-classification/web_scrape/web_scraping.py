71ff7354866ba2afc0113260840f9075abfc2054
henriwoodcock <henriwoodcock@gmail.com>
2019-12-16 22:55:38 +0000

    comment out sppedup

--- automatic-asset-classification/web_scrape/web_scraping.py
+++ automatic-asset-classification/web_scrape/web_scraping.py
@@ -11,8 +11,8 @@ types = ["embankment", "flood_gate", "flood_wall", "outfall", "reservoir", "weir
 
 for type in types:
 
-    if type == "embankment" or type == "flood_wall" or type == "outfall" or type == "flood_gate" or type == "reservior":
-        continue
+    #if type == "embankment" or type == "flood_wall" or type == "outfall" or type == "flood_gate" or type == "reservior":
+    #    continue
 
     output = "data/raw/" + str(type) + "/"
 
@@ -39,3 +39,17 @@ for type in types:
             except Timeout:
                 print(i, "of ", type, "timeout error")
                 logging.exception("Timeout " + str(type) + "row number" + str(i) + "not worked, url: " + url)
+                try:
+                    result = requests.get(url, timeout = 20)
+                    if result.status_code == 200:
+                        image = result.raw.read()
+                        open(output + filename,"wb").write(image)
+                except InvalidSchema:
+                    print(i, "of ", type, "second attempt not worked")
+                    logging.exception("InvalidSchema " + str(type) + "row number" + str(i) + "not worked, url: " + url)
+                except ConnectionError:
+                    print(i, "of ", type, "connection error")
+                    logging.exception("ConnectionError " + str(type) + "row number" + str(i) + "second attempt not worked, url: " + url)
+                except Timeout:
+                    print(i, "of ", type, "timeout error")
+                    logging.exception("Timeout " + str(type) + "row number" + str(i) + "timed out twice, url: " + url)
