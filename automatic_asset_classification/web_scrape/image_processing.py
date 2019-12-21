755f0680455d2263cf582f75e92688b453e9a701
henriwoodcock <henriwoodcock@gmail.com>
2019-12-21 20:31:52 +0000

    created loop to create learners for all types

--- automatic_asset_classification/web_scrape/image_processing.py
+++ automatic_asset_classification/web_scrape/image_processing.py
@@ -1,15 +1,28 @@
 from fastai.vision import *
-image_path = os.getcwd() + "/data/processing/embankment/"
+import matplotlib.pyplot as plt
 
-data = ImageDataBunch.from_folder(image_path, valid_pct = 0.2, size=224,ds_tfms=get_transforms()).normalize(imagenet_stats)
+types = ["embankment", "flood_gate", "flood_wall", "outfall", "reservoir", "weir"]
 
-#resnet 50?
-learn = cnn_learner(data, models.resnet34, metrics=error_rate)
-learn.fit_one_cycle(4)
+learner_dict = dict()
 
-interp = ClassificationInterpretation.from_learner(learn)
-losses,idxs = interp.top_losses()
-len(data.valid_ds)==len(losses)==len(idxs)
-
-interp.plot_top_losses(9, figsize=(15,11))
-interp.plot_confusion_matrix(figsize=(12,12), dpi=60)
+for type_ in types:
+    #get location
+    image_path = os.getcwd() + "/data/processing/" + types_ + "/"
+    #create fastai data bunch for pre processing
+    data = ImageDataBunch.from_folder(image_path, valid_pct = 0.2, size=224,ds_tfms=get_transforms()).normalize(imagenet_stats)
+    #create learner
+    #resnet 50?
+    learn = cnn_learner(data, models.resnet34, metrics=error_rate)
+    #fit on just end layer (other layers are froze)
+    learn.fit_one_cycle(4)
+    #interprate class
+    interp = ClassificationInterpretation.from_learner(learn)
+    losses,idxs = interp.top_losses()
+    len(data.valid_ds)==len(losses)==len(idxs)
+    #validation analysis
+    interp.plot_top_losses(9, figsize=(15,11))
+    interp.plot_confusion_matrix(figsize=(12,12), dpi=60)
+    #add learner to dictionary so can be used for prediction later
+    learner_dict[type_] = learner
+#print plots of validagtion analysis
+plt.show()
