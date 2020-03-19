b69fad342d004b85e0b88ae9d7d10ec5dca0af5a
henriwoodcock <henriwoodcock@gmail.com>
2020-03-19 20:49:18 +0000

    autoencoder updates

--- automatic_asset_classification/experimentation/auto_encoder/resnet_auto_encoder.py
+++ automatic_asset_classification/experimentation/auto_encoder/resnet_auto_encoder.py
@@ -109,7 +109,8 @@ class AutoEncoder(nn.Module):
             nn.BatchNorm2d(16, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
             nn.ReLU(inplace = True),
             nn.Conv2d(16, 3, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False),
-            nn.ReLU(inplace = True)
+            #nn.ReLU(inplace = True)
+            nn.Sigmoid()
         )
 
     def forward(self, x):
